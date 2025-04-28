244b8a6f5d60b846726c1fba95d68021c7b8c56c
Mounica Maddela <mmaddela1005@gmail.com>
2025-04-28 20:55:31 +0000

    Removed outdated code from the repo

--- plan_simp/encode_contexts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python plan_simp/scripts/encode_contexts.py \
-	--data=$1 \
-	--x_col=$3 \
-	--id_col=pair_id \
-	--save_dir=$2/
--- plan_simp/plan_simp/utils.py
+++
@@ -1,4 +0,0 @@
-from typing import Callable, Iterable, List
-
-def lmap(f: Callable, x: Iterable) -> List:
-    return list(map(f, x))
